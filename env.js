f88310d09513599abbd47181ff35f297cc06e162
wxfyes <70796015+wxfyes@users.noreply.github.com>
2024-10-17 16:32:45 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,27 @@
+window.settings = {
+  // 站点标题
+  title: '天阙',
+  // 站点描述
+  description: '没有最好 只有更好',
+  // API
+  host: 'https://go.tianque.cc',
+  // 主题
+  theme: {
+    sidebar: 'dark',
+    header: 'dark',
+    color: 'darkblue'
+  },
+  // 背景
+  background_url: 'https://cdn.jsdelivr.net/gh/wangn9900/tuchuang@main/img/20190815113419267.jpg',
+  // crisp
+  crisp_id: '',
+  i18n: [
+    'zh-CN',
+    'en-US',
+    'ja-JP',
+    'vi-VN',
+    'ko-KR',
+    'zh-TW',
+    'fa-IR'
+  ]
+}
